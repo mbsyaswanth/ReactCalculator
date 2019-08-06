ae674e1592bf8c8d92fddce529bde1d94e5c4237
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-08-06 15:17:28 +0530

    [green state] implemented function to be called with value

--- src/components/Keypad/Button/index.js
+++ src/components/Keypad/Button/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 
 class Button extends Component {
+  handleClick = event => {
+    this.props.action(event.target.value);
+  };
   render() {
     const { action, displayText } = this.props;
     return (
       <div>
-        <input type="button" onClick={action} value={displayText} />
+        <input type="button" onClick={this.handleClick} value={displayText} />
       </div>
     );
   }
